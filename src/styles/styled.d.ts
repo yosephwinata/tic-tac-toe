512b5808a063185981c48d7f67b79aeb38420b28
yosephwinata <yoseph.winata@gmail.com>
2024-02-15 00:11:57 +0700

    Add styled.d.ts

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,25 @@
+// src/styles/styled.d.ts
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    breakpoints: {
+      tablet: string;
+      desktop: string;
+    };
+    colors: {
+      darkCyan: string;
+      cyan: string;
+      lightCyan: string;
+      darkYellow: string;
+      yellow: string;
+      lightYellow: string;
+      veryDarkNavy: string;
+      darkNavy: string;
+      semiDarkNavy: string;
+      gray: string;
+      lightGray: string;
+    };
+    // Add other theme properties as needed
+  }
+}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
+// Important: Remember to update the styled.d.ts file whenever you update the code below.
+// It is required to maximize typescript potentials
 const theme: DefaultTheme = {
   breakpoints: {
     tablet: "48em", // 768px
