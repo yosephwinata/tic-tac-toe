805bead91dccdf03d617e95edafbb3d67921da9f
yosephwinata <yoseph.winata@gmail.com>
2024-02-15 19:44:23 +0700

    Add XO board and squares ui

--- src/features/game/XOBoard.tsx
+++ src/features/game/XOBoard.tsx
@@ -1,8 +1,27 @@
 import styled from "styled-components";
 import XOSquare from "./XOSquare";
 
+const StyledXOBoard = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, auto);
+  row-gap: 2rem;
+  justify-content: space-between;
+`;
+
 const XOBoard: React.FC = () => {
-  return <XOSquare />;
+  return (
+    <StyledXOBoard>
+      <XOSquare />
+      <XOSquare />
+      <XOSquare />
+      <XOSquare />
+      <XOSquare />
+      <XOSquare />
+      <XOSquare />
+      <XOSquare />
+      <XOSquare />
+    </StyledXOBoard>
+  );
 };
 
 export default XOBoard;
